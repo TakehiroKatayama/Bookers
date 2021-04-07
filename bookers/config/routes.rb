2f30c3dd5cd298ebabcb0615e43ace8acc035cc8
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-07 08:02:42 +0000

    [add]モデル、コントローラー、ビュー、ルート

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top'
+
+  root 'homes#top'
+
+  resources :books, only:[:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
