1141da09b9a04a699f65444776fa48acdae109de
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-07 13:46:25 +0000

    [add]create機能

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -7,11 +7,16 @@ class BooksController < ApplicationController
   end
 
   def create
-
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
   end
 
   def edit
   end
 
-
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
