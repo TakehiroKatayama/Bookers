ba0063e999b990387c11fa8c5cf524cfdc1201a8
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-07 11:55:31 +0000

    [add]topページ

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'homes#top'
 
-  resources :books, only:[:new]
+  resources :books, except:[:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
