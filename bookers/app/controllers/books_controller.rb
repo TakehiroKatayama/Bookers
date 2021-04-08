2212fe2a6df9850977b5709eba16470a40238de1
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-08 05:05:07 +0000

    [add]create_flash_message

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -8,10 +8,8 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+  
+
   end
 
   def edit
