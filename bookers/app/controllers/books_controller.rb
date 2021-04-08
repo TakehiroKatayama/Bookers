bee37e8d1de1696f9351b39e2a72da8c9a51b192
TakehiroKatayama <t.katayama.info@gmail.com>
2021-04-08 07:41:13 +0000

    [add]update_validates

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -26,12 +26,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:success] = "Book was succesfully updated."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
     else
-
+      render :edit
     end
   end
 
